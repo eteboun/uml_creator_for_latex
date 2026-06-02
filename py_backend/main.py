0123e5872f39008d2c1ecca84c25421e2127abf4
user123 <talhaegeerbay@gmail.com>
2026-06-02 17:24:08 +0300

    building java backend

--- py_backend/main.py
+++
@@ -1,9 +0,0 @@
-from api.session import Session
-
-session = Session(
-    allowed_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://192.168.35.76:5173",
-    ]
-)
